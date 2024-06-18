e9f4dd8e483cdfc3c4fc7a7a851bd49f70c9704a
juliocesar <juliofreitas.bm@gmail.com>
2024-06-18 08:25:41 -0300

    Teste E2E do perfil

--- src/http/controllers/users/authenticate.spec.ts
+++ src/http/controllers/users/authenticate.spec.ts
@@ -2,7 +2,7 @@ import { app } from '@/app'
 import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe('Register (e2e)', () => {
+describe('Authenticate (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -11,7 +11,7 @@ describe('Register (e2e)', () => {
     await app.close()
   })
 
-  it('sould be able to register', async () => {
+  it('should be able to authenticate', async () => {
     const response = await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'johndoe@example.com',
